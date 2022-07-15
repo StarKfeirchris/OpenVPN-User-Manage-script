cf87f3fefc813866a215e3d9586e409466774458
Star Kfeirchris <30791968+StarKfeirchris@users.noreply.github.com>
2022-07-15 21:01:37 +0800

    🐛 Correct sentences

--- ovpn-user-manage.sh
+++ ovpn-user-manage.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
-# Release v3 ; last upsate: 20201208
+# Release v4.5 ; last upsate: 20220715
 
-# Setting the execution environment, debug: set -xeo pipefail
+# Set the execution environment, debug: set -xeo pipefail
 set -eo pipefail
 
-# Definition color; E = Color end
+# Define the color; E = Color end
 R="\e[1;91m"
 G="\e[1;92m"
 Y="\e[1;93m"
 B="\e[1;96m"
 E="\e[0m"
 
-# Chenge script location.
+# Chenge the script execution folder.
 cd /etc/openvpn/easy-rsa/2.0/
 
-# Check system virsion and package.
+# Check the system version and package.
 os=$(lsb_release -irs | xargs)
 if [[ ${os} == 'CentOS '* ]];
 then
-	check_rpm=$(rpm -qa | grep "p7zip" -c || true)
-	if [[ ${check_rpm} == 2 ]];
+	check_rpm=$(rpm -qa | grep -E "p7zip|cifs" -c || true)
+	if [[ ${check_rpm} == 3 ]];
 	then
 		true
 	else
-		yum install p7zip p7zip-plugins -y
+		yum install p7zip p7zip-plugins cifs-utils -y
 	fi
 elif [[ ${os} == 'Ubuntu '* ]];
 then
-	check_dpkg=$(dpkg --get-selections | grep "p7zip" -c || true)
-	if [[ ${check_dpkg} == 1 ]];
+	check_dpkg=$(dpkg --get-selections | grep -E "p7zip|cifs" -c || true)
+	if [[ ${check_dpkg} == 2 ]];
 	then
 		true
 	else
-		apt-get install p7zip-full -y
+		apt-get install p7zip-full cifs-utils -y
 	fi
 else
 	echo -e "${R}This system is not CentOS or Ubuntu!${E}"
@@ -44,8 +44,8 @@ fi
 ovpn_config_head=$(echo 'client
 dev tun
 proto udp
-remote vpn2.vpnserveraddress.com 1194
-remote vpn.vpnserveraddress.com 1194
+remote vpn.vpnserver.com 1194
+remote vpn2.vpnserver.com 1194
 nobind
 persist-key
 persist-tun
@@ -57,7 +57,7 @@ auth SHA256
 ')
 
 # Select Add, Revoke or search user.
-# If the question option is entered incorrectly, it will be asked again.
+# If the incorrect option is entered, it will be asked again.
 while read -p "
 $(echo -e "${Y}0${E} > ${G}Search user${E}")
 $(echo -e "${Y}1${E} > ${G}Add user${E}")
@@ -66,8 +66,8 @@ $(echo -e "${Y}D/d${E} > ${R}Delete user certificate${E}")
 
 $(echo -e "${B}Please choose one >${E}") " feature_choose
 
-# Enter user name.
-read -p "$(echo -e "${B}Please enter user name:${E}") " ovpn_user
+# Enter the user name.
+read -p "$(echo -e "${B}Please enter the user name:${E}") " ovpn_user
 
 # get_user_avl: avl = available ; get_user_rm: rm = remove
 # "grep -c" = Suppress normal output; instead print a count of matching lines for each input file.
@@ -79,14 +79,14 @@ do
 	then
 		search_user_file=$(ls /etc/openvpn/easy-rsa/2.0/keys/ | grep -wc ${ovpn_user}.key || true)
 
-		echo -e "User Name: ${Y}${ovpn_user}${E}"
+		echo -e "User name: ${Y}${ovpn_user}${E}"
 
-		# Check user file.
+		# Check user files.
 		if [[ ${search_user_file} == 1 ]];
 		then
-			echo -e "File exist: ${G}Ｏ${E}"
+			echo -e "File exists: ${G}Ｏ${E}"
 		else
-			echo -e "File exist: ${R}Ｘ${E}"
+			echo -e "File exists: ${R}Ｘ${E}"
 		fi
 
 		# Check user account.
@@ -100,20 +100,20 @@ do
 		then
 			echo -e "Available: ${R}Ｘ${E}"
 		else
-			echo -e "Available: ${R}Not exist!${E}"
+			echo -e "Available: ${R}not exists!${E}"
 		fi
 		break
 	elif [[ ${feature_choose} == 1 ]];
 	then
-		# Confirm that the user has not been created.
+		# Confirm whether the user has been created.
 		check_username=$(ls /etc/openvpn/easy-rsa/2.0/keys/ | grep ${ovpn_user}.key -c || true)
 		if [[ ${check_username} == 1 ]];
 		then
-			echo -e "${B}Find same user:${E} ${Y}${ovpn_user}${E}${B} !  please check ${E}${R}/etc/openvpn/user/${E}"
+			echo -e "${B}Find the same user:${E} ${Y}${ovpn_user}${E}${B} !  please check ${E}${R}/etc/openvpn/user/${E}"
 			exit 0
 		fi
 
-		# Randomly generate certificate password.
+		# Randomly generate a certificate password.
 		char_1=$(echo ${ovpn_user} | cut -c1)
 		char_cap_1=$(echo ${ovpn_user} | cut -c1 | tr '[:lower:]' '[:upper:]')
 		char_2=$(echo ${ovpn_user} | cut -c2)
@@ -121,20 +121,20 @@ do
 		cert_pass=$(echo ${char_1}${char_2}${py_pass}${char_cap_1}${char_2})
 		echo -e "${B}Recommended certificate password:${E} ${Y}${cert_pass}${E}"
 
-		# Start create new user <new username>.ovpn file.
+		# Start creating new user <new username>.ovpn files.
 		source /etc/openvpn/easy-rsa/2.0/vars
 		sh /etc/openvpn/easy-rsa/2.0/build-key-pass ${ovpn_user}
 				
-		# Make new user folder for certificate file.
+		# Create folders for new user.
 		mkdir /etc/openvpn/user/${ovpn_user}
 				
-		# Copy new user certificate file to /etc/openvpn/user/<new username>
+		# Copy the new user certificate file to /etc/openvpn/user/<new username>
 		\cp /etc/openvpn/easy-rsa/2.0/keys/${ovpn_user}.* /etc/openvpn/user/${ovpn_user}
 				
-		# Copy OpenVPN config to new user <new username>.ovpn file.
+		# Copy the OpenVPN config to <new username>.ovpn file.
 		echo -e "${ovpn_config_head}\n" >> /etc/openvpn/user/${ovpn_user}/${ovpn_user}.ovpn
 		
-		# Copy new user certificate to <new username>.ovpn file.
+		# Copy the new user certificate to <new username>.ovpn file.
 		echo '<ca>' >> /etc/openvpn/user/${ovpn_user}/${ovpn_user}.ovpn
 		cat /etc/openvpn/easy-rsa/2.0/keys/ca.crt >> /etc/openvpn/user/${ovpn_user}/${ovpn_user}.ovpn
 		echo '</ca>' >> /etc/openvpn/user/${ovpn_user}/${ovpn_user}.ovpn
@@ -147,45 +147,46 @@ do
 		cat /etc/openvpn/user/${ovpn_user}/${ovpn_user}.key >> /etc/openvpn/user/${ovpn_user}/${ovpn_user}.ovpn
 		echo '</key>' >> /etc/openvpn/user/${ovpn_user}/${ovpn_user}.ovpn
 		
-		# Enter <new username>.ovpn password again & Create Readme.txt.
-		read -p "$(echo -e "${B}Please enter the${E} ${R}password${E} ${B}for${E} ${Y}${ovpn_user}${E}${B} certificate:${E}") " readme_text
+		# Enter the password of <new username>.ovpn again & create Readme.txt.
+		read -p "$(echo -e "${B}Please enter the${E} ${R}certificate password${E} ${B}of${E} ${Y}${ovpn_user}${E}${B} :${E}") " readme_text
 		echo ${readme_text} >> /etc/openvpn/user/${ovpn_user}/Readme.txt
 
 		# Create archive password & archive file, "-mhe" = Encryption filename extension.
-		zip_pw=$(echo Sk@$RANDOM$RANDOM)
+		zip_pw=$(echo Bw@$RANDOM$RANDOM)
 		text_file="/etc/openvpn/user/${ovpn_user}/Readme.txt"
 		ovpn_file="/etc/openvpn/user/${ovpn_user}/${ovpn_user}.ovpn"
-		7z a -mhe -p${zip_pw} ${ovpn_user}.7z ${text_file} ${ovpn_file}
+		#7z a -mhe -p${zip_pw} ${ovpn_user}.7z ${text_file} ${ovpn_file}
+		7z a -p${zip_pw} ${ovpn_user}.7z ${text_file} ${ovpn_file}
 
-		# Confirm that the archive file copy is successful.
-		check_file=$(ls | grep ${new_user}.7z -c || true)
+		# Confirm that the archive file has been successfully copied.
+		check_file=$(ls | grep ${ovpn_user}.7z -c || true)
 		if [[ ${check_file} == 1 ]];
 		then
 			true
 		else
-			echo -e "${Y}${new_user}.7z${E} ${B}does not exist, please check.${E}"
+			echo -e "${Y}${ovpn_user}.7z${E} ${B}does not exist, please check.${E}
 			exit 0
 		fi
 
 		# Remove Readme.txt
 		rm -f /etc/openvpn/user/${ovpn_user}/Readme.txt
 		
-		# Show archive file password to screen and finished.
-		echo -e "${G}${new_user}.7z${E} ${B}is here.${E}"
-		echo -e "${G}${new_user}.7z${E} ${B}password is${E} ${Y}${zip_pw}${E} ${B}, add user finished.${E}"
+		# Show the archive file password to the screen and finished.
+		echo -e "${G}${ovpn_user}.7z${E} ${B}is here.${E}"
+		echo -e "${G}${ovpn_user}.7z${E} ${B}password is${E} ${Y}${zip_pw}${E} ${B}, add user finished.${E}"
 		break
 	elif [[ ${feature_choose} == 2 ]];
 	then
-		# Confirm user exist.
+		# Confirm the existence of users.
 		check_username=$(ls /etc/openvpn/user | grep ${ovpn_user} -c || true)
 		if [[ ${check_username} == 0 ]];
 		then
-			echo -e "${B}Can not be found${E} ${Y}${ovpn_user}${E}${B} !  please check ${E}${R}/etc/openvpn/user/${E}"
+			echo -e "${B}Can't find${E} ${Y}${ovpn_user}${E}${B} !  please check ${E}${R}/etc/openvpn/user/${E}"
 			exit 0
 		fi
 
-		# Confirm revoke user.
-		read -p "$(echo -e "${R}Are you sure continue revoke${E} ${Y}${ovpn_user}${E}${R} ?(Y/N)${E}") " confirm_revoke
+		# Confirm the revoke of users.
+		read -p "$(echo -e "${R}Are you sure want to continue to revoke${E} ${Y}${ovpn_user}${E}${R} ?(Y/N)${E}") " confirm_revoke
 		if [[ ${confirm_revoke} == Y || ${confirm_revoke} == y ]];
 		then
 			true
@@ -198,18 +199,18 @@ do
 			exit 0
 		fi
 
-		# Start revoke user certificate.
+		# Start to revoke the user certificate.
 		source /etc/openvpn/easy-rsa/2.0/vars
 		sh /etc/openvpn/easy-rsa/2.0/revoke-full ${ovpn_user}
 		break
 	elif [[ ${feature_choose} == D || ${feature_choose} == d ]];
 	then
-		# Show warning message and confirm continue.
+		# Show warning messages and confirm to continue.
 		echo -e "${R}!!!!!WARNING!!!!!${E}"
-		echo -e "${R}This option will delete the user certificate!${E}"
+		echo -e "${R}This option will delete the user's certificate!${E}"
 		echo -e "${R}Please think twice!${E}"
 		echo
-		read -p "$(echo -e "${B}Are you sure continue?(Y/N)${E}") " del_confirm
+		read -p "$(echo -e "${B}Are you sure want to continue?(Y/N)${E}") " del_confirm
 
 		if [[ ${del_confirm} == Y || ${del_confirm} == y ]];
 		then
@@ -222,23 +223,23 @@ do
 			exit 0
 		fi
 
-		# Confirm user has been revoked. 
+		# Confirm that the user has been revoked. 
 		if [[ ${get_user_rm} == 0 ]];
 		then
-			echo -e "${B}No such user or user ${E}${Y}${ovpn_user}${E}${B} certificate has not revoked.${E}"
+			echo -e "${B}No such user or the user ${E}${Y}${ovpn_user}${E}${B} certificate has not been revoked.${E}"
 			exit 0
 		fi
 
-		# Last check, enter "y" is start remove user certificate, enter "n" is stop.
-		echo -e "${B}You are now to delete the certificate of ${E}${Y}${ovpn_user}${E}${B},${E}"
+		# Finally confirm the enter "y" to start removeing the user certificate, and enter "n" to stop.
+		echo -e "${B}You are going to delete the certificate of ${E}${Y}${ovpn_user}${E}${B} now,${E}"
 		read -p "$(echo -e "${R}Are you sure?(Y/N)${E}") " last_check
 		if [[ ${last_check} == Y || ${last_check} == y ]];
 		then
 			rm -rf /etc/openvpn/user/${ovpn_user}
 			rm -f /etc/openvpn/easy-rsa/2.0/keys/${ovpn_user}.*
-			echo -e "${B}Delete certificate is done. ${E}"
+			echo -e "${B}The deletion certificate has been finished. ${E}"
 		else
-			echo -e "${B}Okay...you give up.${E}"
+			echo -e "${B}Okay...good choice.${E}"
 			exit 0
 		fi
 		break
@@ -249,4 +250,5 @@ do
 	fi
 	continue
 done
+
 
